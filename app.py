ba1dd007282316ffbe55ba8403c7ba6fb63de548
mayur05072000 <46949845+mayur05072000@users.noreply.github.com>
2021-04-28 15:18:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Vachana18/lexus'
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
